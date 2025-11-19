d232f7a3fcc08235416d5c9dd36aa8e489c0dcf4
Jacob Simmons <jakerstx@gmail.com>
2025-11-18 23:54:46 -0600

    Add setup_player file for cleaner startup; cache player_info into a json for use in other scripts.

--- api/sleeper_api.py
+++ api/sleeper_api.py
@@ -2,11 +2,12 @@ import requests
 import json
 
 class SleeperAPI:
-    BASE_URL = "https://api.sleeper.app/v1/league/1291191281669644288"
+    DEFAULT_LEAGUE_ID = "1291191281669644288"
+    BASE_URL = f"https://api.sleeper.app/v1/league/{DEFAULT_LEAGUE_ID}"
 
     @staticmethod
-    def get_week_matchups(week):
-        url = f"{SleeperAPI.BASE_URL}/matchups/{week}"
+    def get_week_matchups(league_id, week):
+        url = f"https://api.sleeper.app/v1/league/{league_id}/matchups/{week}"
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
@@ -14,9 +15,9 @@ class SleeperAPI:
             raise Exception(f"Failed to fetch data: {response.status_code}")
 
     @staticmethod
-    def get_my_team_data(data, team_id):
+    def get_my_team_data(data, roster_id):
         for matchup in data:
-            if matchup['roster_id'] == team_id:
+            if matchup['roster_id'] == roster_id:
                 return matchup
         raise Exception("Team ID not found in the data")
 
@@ -32,3 +33,67 @@ class SleeperAPI:
         with open("nba_players.json", "r", encoding="utf-8") as f:
             id_to_name = json.load(f)
         return id_to_name.get(sleeper_id, "Unknown Player")
+    
+    @staticmethod
+    def get_league_info(league_id):
+        url = f"https://api.sleeper.app/v1/league/{league_id}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to fetch league info: {response.status_code}")
+        
+    
+
+    @staticmethod
+    def get_rosters(league_id):
+        url = f"https://api.sleeper.app/v1/league/{league_id}/rosters"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to fetch rosters: {response.status_code}")
+        
+    @staticmethod
+    def get_users_roster_id(league_id, user_id):
+        url = f"https://api.sleeper.app/v1/league/{league_id}/rosters"
+        response = requests.get(url)
+        if response.status_code == 200:
+            rosters = response.json()
+            for roster in rosters:
+                if roster['owner_id'] == user_id:
+                    return roster['roster_id']
+            raise Exception("User ID not found in rosters")
+        else:
+            raise Exception(f"Failed to fetch rosters: {response.status_code}")
+    
+
+    @staticmethod
+    def get_user_id_from_username(username):
+        url = f"https://api.sleeper.app/v1/user/{username}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            user_data = response.json()
+            return user_data['user_id']
+        else:
+            raise Exception(f"Failed to fetch user ID: {response.status_code}")
+        
+
+    @staticmethod
+    def get_leagues_for_user(user_id, season="2025"):
+        url = f"https://api.sleeper.app/v1/user/{user_id}/leagues/nba/{season}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to fetch leagues: {response.status_code}")
+        
+
+    @staticmethod
+    def get_league_id():
+        return SleeperAPI.DEFAULT_LEAGUE_ID
+    
+    @staticmethod
+    def set_league_id(league_id):
+        SleeperAPI.DEFAULT_LEAGUE_ID = league_id
+        SleeperAPI.BASE_URL = f"https://api.sleeper.app/v1/league/{league_id}"
---
+++ get_weekly_stats.py
@@ -0,0 +1,67 @@
+import json
+import os
+from api.sleeper_api import SleeperAPI
+from models.fantasy_data import FantasyData
+from simulation.simulation import FantasyNBASimulation
+from utils.helpers import (
+    get_my_team_and_opponent_team,
+    get_player_names_from_team_data,
+    get_week_data_filename,
+    player_names_to_fantasy_stats,
+    get_current_week
+)
+import time
+import questionary
+
+
+def main():
+
+
+    # load the player stats from player_info.json if it exists
+    player_info = None
+    if os.path.exists("player_info.json"):
+        print("Loading player info from player_info.json...")
+        with open("player_info.json", "r", encoding="utf-8") as f:
+            player_info = json.load(f)
+    if player_info:
+        print(f"Loaded Player Info for user {player_info['username']} (User ID: {player_info['user_id']})")
+        print("\n")
+    else:
+        print("No player info found. Please run setup_player.py first.")
+        return
+    
+
+    default_week = get_current_week()
+    week = questionary.text("Enter the week number:", default=str(default_week)).ask()
+    week = int(week)
+
+    roster_id = player_info['roster_id']
+    league_id = player_info['main_league_id']
+
+
+
+    matchups = SleeperAPI.get_week_matchups(league_id, week)
+
+    user_matchup_data, opp_matchup_data = get_my_team_and_opponent_team(roster_id, matchups)
+
+
+    my_player_names = get_player_names_from_team_data(user_matchup_data)
+    opponent_player_names = get_player_names_from_team_data(opp_matchup_data)
+
+    my_team_fantasy_stats = player_names_to_fantasy_stats(my_player_names)
+    opponent_team_fantasy_stats = player_names_to_fantasy_stats(opponent_player_names)
+
+    print("My Team Fantasy Stats:")
+    for name, stats in my_team_fantasy_stats.items():
+        print(f"{name}: Mean = {stats[0]:.2f}, StdDev = {stats[1]:.2f}, games played = {stats[2]}")
+
+    print("\nOpponent Team Fantasy Stats:")
+    for name, stats in opponent_team_fantasy_stats.items():
+        print(f"{name}: Mean = {stats[0]:.2f}, StdDev = {stats[1]:.2f}, games played = {stats[2]}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
